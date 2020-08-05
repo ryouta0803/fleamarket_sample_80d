72aa08e098d828e05277d2e81e52ffe5fda0fed3
katopu <kato-d.home@outlook.jp>
2020-08-05 16:42:49 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "fleamarket_sample_80d"
-set :repo_url, "git@example.com:katopu/fleamarket_sample_80d.git"
+set :repo_url, "git@github.com:katopu/fleamarket_sample_80d.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
