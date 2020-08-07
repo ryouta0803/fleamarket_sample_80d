8fe0d86b34014cd1c277ddf2a8b651bc85839e29
katopu <kato-d.home@outlook.jp>
2020-08-07 21:45:16 +0900

    Revert "log in機能実装"
    This reverts commit 1ebe0db7a49c13d129bfea6b349a3ec6eb9734dd.

--- app/models/address.rb
+++
@@ -1,6 +0,0 @@
-class Address < ApplicationRecord
-  belongs_to :user, optional: true
-  validates :first_name, :last_name, :postal_code, :first_name_kana, :last_name_kana, :postal_code, :prefecture, :city, :address, presence: true
-  validates :first_name, :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name_kana, :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
-end
