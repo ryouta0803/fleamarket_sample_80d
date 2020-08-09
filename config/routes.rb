dcb5de1b537bf7120fba1fe42808dec83b245c5c
ryouta0803 <67770359+ryouta0803@users.noreply.github.com>
2020-08-09 21:48:54 +0900

    ルートを作成

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'items#index'
   # resources :users, only: [:edit, :update]
+  resources :cards, only: [:new, :show, :destroy] do
+    collection do
+      post 'pay', to: 'cards#pay'
+    end
+  end
 end
