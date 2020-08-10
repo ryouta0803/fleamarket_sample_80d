ecdcb1199921c39f8a852b470d3e492e7b7391ec
kimkim2951 <ty53.k.k@gmail.com>
2020-08-10 10:56:38 +0900

    [WIP]商品出品機能サーバーサイド 　brandsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_062129) do
+ActiveRecord::Schema.define(version: 2020_08_10_013609) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "category", null: false
