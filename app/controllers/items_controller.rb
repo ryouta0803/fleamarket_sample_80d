3d7af04c89da902c159e3dc8f94f2bb4baedbea1
katopu <kato-d.home@outlook.jp>
2020-08-15 21:38:04 +0900

    修正点更新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,11 +38,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    # @item = Item.find(params[:id])
   end
 
   def update
-    # @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
