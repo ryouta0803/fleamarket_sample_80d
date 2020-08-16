ae7d7d6a8c7f8c2cbdb46a825dca0cab98a36c86
ryouta0803 <67770359+ryouta0803@users.noreply.github.com>
2020-08-16 21:46:27 +0900

    カードモデルのバリデーション

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,9 @@
 class Card < ApplicationRecord
   belongs_to :user
+  validates :customer_token, presence: true
+  validates :card_number, :card_company, :card_year, :card_month,:card_pass, presence: true
+  validates :card_number,:card_pass, numericality: { only_integer: true }
+  validates :card_number, length: { is: 16 }
+  validates :card_pass, length: { in: 3..4 }
+
 end
