b24cf503dd65e1ba3878aad867dc1faef6216396
ryouta0803 <67770359+ryouta0803@users.noreply.github.com>
2020-08-18 14:27:20 +0900

    ルーティングの整理、ヘッダーカテゴリの変更、商品詳細に購入ページへのパス追加

--- config/routes.rb
+++ config/routes.rb
@@ -13,29 +13,29 @@ Rails.application.routes.draw do
   # resources :users, only: [:edit, :update]
   resources :cards, only: [:new, :show, :destroy] do
 
-  resources :items do
-    # resources :comments 準備のみ
-    #Ajaxで動くアクションのルートを作成
-    collection do
-      post 'pay', to: 'cards#pay'
-      post 'purchase', to: 'items#purchase'
+    resources :items do
+      # resources :comments 準備のみ
+      #Ajaxで動くアクションのルートを作成
+      collection do
+        post 'pay', to: 'cards#pay'
+        post 'purchase', to: 'items#purchase'
+      end
     end
   end
-  
+
   resources :items
     #Ajaxで動くアクションのルートを作成
-    resources :items, only: [:new, :show, :create, :edit, :update, :destroy] do
+  resources :items, only: [:new, :show, :create, :edit, :update, :destroy] do
 
+    collection do
+      get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
+      get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
+    end
+    
+    resources :buyers, only: [:index] do
       collection do
-        get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
-        get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
-      end
-      
-      resources :buyers, only: [:index] do
-        collection do
-          get 'done', to: 'buyers#done'
-          post 'pay', to: 'buyers#pay'
-        end
+        get 'done', to: 'buyers#done'
+        post 'pay', to: 'buyers#pay'
       end
     end
   end
