d4f7c9aa4500e84ac4584f8e7bca4c3cb482adae
kimkim2951 <ty53.k.k@gmail.com>
2020-08-19 17:08:01 +0900

    [fix]カテゴリーJS修正

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $(function(){
     function appendOption(category){
-      var html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
+      let html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
       return html;
     }
     // 子カテゴリーを表示させる。
     function appendChildrenBox(insertHTML){
-      var childSelectHtml = '';
+      let childSelectHtml = '';
       childSelectHtml = `<div class='exhibitionPage__main__contents__detail__category__choose__added' id= 'children_wrapper'>
                           <div class='exhibitionPage__main__contents__detail__category__choose1'>
                             <select class="exhibitionPage__main__contents__detail__category__choose--select" id="child_category" name="item[category_id]">
@@ -19,7 +19,7 @@ $(document).ready(function(){
     }
     // 孫カテゴリーを表示させる。
     function appendGrandchildrenBox(insertHTML){
-      var grandchildSelectHtml = '';
+      let grandchildSelectHtml = '';
       grandchildSelectHtml = `<div class='exhibitionPage__main__contents__detail__category__choose__added' id= 'grandchildren_wrapper'>
                                 <div class='exhibitionPage__main__contents__detail__category__choose2'>
                                   <select class="exhibitionPage__main__contents__detail__category__choose__box--select" id="grandchild_category" name="item[category_id]">
@@ -34,7 +34,7 @@ $(document).ready(function(){
     console.log($('#parent_category'));
     $('#parent_category').on('change', function(){
       // 選択された親カテゴリーのidを取得
-      var parent_category_id = document.getElementById
+      let parent_category_id = document.getElementById
       ('parent_category').value; 
       if (parent_category_id != "---"){ 
         $.ajax({
@@ -49,7 +49,7 @@ $(document).ready(function(){
           $('#children_wrapper').remove(); 
           $('#grandchildren_wrapper').remove();
 
-          var insertHTML = '';
+          let insertHTML = '';
           children.forEach(function(child){
             insertHTML += appendOption(child);
           });
@@ -68,7 +68,7 @@ $(document).ready(function(){
     });
 
     $('.content-sale__main__box__form').on('change', '#child_category', function(){
-      var child_category_id = $('#child_category option:selected').data('category'); 
+      let child_category_id = $('#child_category option:selected').data('category'); 
       if (child_category_id != "---"){ 
         $.ajax({
           url: '/items/category/get_category_grandchildren',
@@ -80,7 +80,7 @@ $(document).ready(function(){
           if (grandchildren.length != 0) {
             // 子カテゴリーが変更された時、孫カテゴリーを削除する。
             $('#grandchildren_wrapper').remove(); 
-            var insertHTML = '';
+            let insertHTML = '';
             grandchildren.forEach(function(grandchild){
               insertHTML += appendOption(grandchild);
             });
