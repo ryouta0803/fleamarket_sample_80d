d2b2616a2a2c0f036ce6eabe02ae160afbc69cd6
mas-i <fd27763s@gmail.com>
2020-08-20 16:32:15 +0900

    クレジットカード登録後の遷移先の記述を変更
    1. buyers_controllerのbefore_actionにindex,pay追記
    2. cards.controllerの22行目pathを変更

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -19,7 +19,7 @@ class CardsController < ApplicationController
       ) 
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to card_path(current_user.id)
+        redirect_to item_buyers_path(params[:item_id])
         flash[:notice] = 'クレジットカードの登録が完了しました'
       else
         redirect_to pay_cards_path
