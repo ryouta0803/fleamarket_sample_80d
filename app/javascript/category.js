b8f540fee7efacc4e9543426559f7d4c4f3f3045
mas-i <fd27763s@gmail.com>
2020-08-21 17:58:47 +0900

    商品購入ボタン条件分岐追加,変更
    ログインユーザーのみ購入ボタンが表示される。
    ログアウトユーザーはログイン,新規登録ボタンが表示される。

--- app/javascript/category.js
+++ app/javascript/category.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
   $(function(){
     function appendOption(category){
       let html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
